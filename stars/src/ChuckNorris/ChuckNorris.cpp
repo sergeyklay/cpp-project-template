848d46b992017c82c0dff779e70b67e3a61374a5
Serghei Iakovlev <egrep@protonmail.ch>
2020-09-15 02:05:09 +0300

    Reorder includes

--- stars/src/ChuckNorris/ChuckNorris.cpp
+++ stars/src/ChuckNorris/ChuckNorris.cpp
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <sqlite3.h>
 #include <spdlog/spdlog.h>
+#include <sqlite3.h>
 
 #include <stars/ChuckNorris.hpp>
 
